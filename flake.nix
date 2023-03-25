2f8c574044ef4e5e11b9ffa32b523cc08059710d
Renzo Carbonara <gnuk0001@gmail.com>
2023-03-25 10:18:24 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -1,70 +1,52 @@
 {
-  description = "Haskell memzero library";
+  description = "Haskell 'memzero' library";
 
-  outputs = { self, nixpkgs }:
-    let
-      hsMemzero = import ./.;
-      hspkgsOverrides = pself: psuper: hself: hsuper: {
-        memzero = hsuper.callPackage hsMemzero { };
-      };
-      pkgsOverlay = pself: psuper: {
-        haskell = psuper.haskell // {
-          packageOverrides = hspkgsOverrides pself psuper;
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs?rev=21eda9bc80bef824a037582b1e5a43ba74e92daa";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
+      perSystem = { config, pkgs, final, ... }: {
+        overlayAttrs = {
+          haskell = pkgs.haskell // {
+            packageOverrides = pkgs.lib.composeExtensions
+              (pkgs.haskell.packageOverrides or (_: _: { }))
+              (hself: hsuper: { memzero = hself.callPackage ./. { }; });
+          };
         };
-      };
-      pkgsFor = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ pkgsOverlay ];
+        packages = {
+          default = pkgs.releaseTools.aggregate {
+            name = "every output from this flake";
+            constituents = [
+              config.packages.memzero__ghc943
+              config.packages.memzero__ghc943.doc
+              config.devShells.ghc943
+
+              config.packages.memzero__ghc925
+              config.packages.memzero__ghc925.doc
+              config.devShells.ghc925
+            ];
+          };
+          memzero__ghc925 = final.pkgs.haskell.packages.ghc925.memzero;
+          memzero__ghc943 = final.pkgs.haskell.packages.ghc943.memzero;
         };
-
-    in {
-      inherit hsMemzero hspkgsOverrides pkgsOverlay;
-      packages =
-        nixpkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ]
-        (system:
-          let pkgs = pkgsFor system;
-          in {
-            default = pkgs.releaseTools.aggregate {
-              name = "every output from this flake";
-              constituents = let
-                p = self.packages.${system};
-                s = self.devShells.${system};
-              in [
-                p.hs_memzero__ghcDefault
-                p.hs_memzero__ghc925
-                p.hs_memzero__ghc943
-
-                p.hs_memzero__ghcDefault.doc
-                p.hs_memzero__ghc925.doc
-                p.hs_memzero__ghc943.doc
-
-                s.hs_memzero__ghcDefault
-                s.hs_memzero__ghc925
-                s.hs_memzero__ghc943
-              ];
+        devShells = let
+          shellFor = hpkgs:
+            hpkgs.shellFor {
+              packages = p: [ p.memzero ];
+              withHoogle = true;
+              nativeBuildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
             };
-            hs_memzero__ghcDefault = pkgs.haskellPackages.memzero;
-            hs_memzero__ghc925 = pkgs.haskell.packages.ghc925.memzero;
-            hs_memzero__ghc943 = pkgs.haskell.packages.ghc943.memzero;
-          });
-      devShells =
-        nixpkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ]
-        (system:
-          let
-            pkgs = pkgsFor system;
-            mkShellFor = hpkgs:
-              hpkgs.shellFor {
-                packages = p: [ p.memzero ];
-                withHoogle = true;
-                nativeBuildInputs = [ pkgs.cabal-install pkgs.cabal2nix ];
-              };
-          in {
-            default = self.devShells.${system}.hs_memzero__ghcDefault;
-            hs_memzero__ghcDefault = mkShellFor pkgs.haskellPackages;
-            hs_memzero__ghc925 = mkShellFor pkgs.haskell.packages.ghc925;
-            hs_memzero__ghc943 = mkShellFor pkgs.haskell.packages.ghc943;
-          });
+        in {
+          default = config.devShells.ghc943;
+          ghc925 = shellFor final.pkgs.haskell.packages.ghc925;
+          ghc943 = shellFor final.pkgs.haskell.packages.ghc943;
+        };
+      };
     };
-
 }
